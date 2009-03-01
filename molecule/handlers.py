a37b46675d067c299faae8601434b791a0f24f15
lxnay <lxnay@cd1c1023-2f26-0410-ae45-c471fc1f0318>
2009-03-01 19:53:01 +0000

    Molecule: - mkisofs accepts Volume IDs at most 32 chars long
    git-svn-id: http://svn.sabayonlinux.org/projects/molecule/trunk@3097 cd1c1023-2f26-0410-ae45-c471fc1f0318

--- molecule/handlers.py
+++ molecule/handlers.py
@@ -473,7 +473,7 @@ class IsoHandler(GenericHandlerInterface):
         args.extend(self.Config['iso_builder_builtin_args'])
         args.extend(self.metadata.get('extra_mkisofs_parameters',[]))
         if self.iso_title.strip():
-            args.extend(["-V",'"',self.iso_title,'"'])
+            args.extend(["-V",'"',self.iso_title[:32],'"'])
         args.extend(['-o',self.dest_iso,self.source_path])
         self.Output.updateProgress("[%s|%s] %s: %s" % (
                 blue("IsoHandler"),darkred(self.spec_name),
