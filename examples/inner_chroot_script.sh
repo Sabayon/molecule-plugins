3299150e048e8f57fb41fd18447b28327c0e1d78
Fabio Erculiani <fabio.erculiani@itsme.it>
2009-08-12 15:48:17 +0200

    [examples] update inner_chroot_script sample file

--- examples/inner_chroot_script.sh
+++ examples/inner_chroot_script.sh
@@ -4,13 +4,14 @@
 source /etc/profile
 
 # allow root logins to the livecd by default
+
 # turn bashlogin shells to actual login shells
 sed -i 's:exec -l /bin/bash:exec -l /bin/bash -l:' /bin/bashlogin
 
 # setup sudoers
 [ -e /etc/sudoers ] && sed -i '/NOPASSWD: ALL/ s/^# //' /etc/sudoers
 
-# setup opengl
+# setup opengl in /etc (if configured)
 eselect opengl set xorg-x11
 
 # touch /etc/asound.state
@@ -30,24 +31,25 @@ if [ "$myuname" == "x86_64" ]; then
         mydir="amd64"
 fi
 
-rm -rf /var/lib/entropy/packages/${mydir}/4/x11-drivers*
+rm -rf /var/lib/entropy/packages/${mydir}/5/x11-drivers*
 ACCEPT_LICENSE="NVIDIA" equo install --fetch --nodeps =x11-drivers/nvidia-drivers-173*
 ACCEPT_LICENSE="NVIDIA" equo install --fetch --nodeps =x11-drivers/nvidia-drivers-96*
 # dropped since no X 1.5 support yet
 # ACCEPT_LICENSE="NVIDIA" equo install --fetch --nodeps ~x11-drivers/nvidia-drivers-71.86.07
 mv /var/lib/entropy/packages/${mydir}/4/x11-drivers\:nvidia-drivers*.tbz2 /install-data/drivers/
 
-# Remove Jenna ?
-sed -i '/jenna/ s/.*//' /etc/entropy/repositories.conf
-rm -rf /var/lib/entropy/client/database/amd64/jenna
-rm -rf /var/lib/entropy/client/database/x86/jenna
-
 # Add fusion icon to desktop
 if [ -f "/usr/share/applications/fusion-icon.desktop" ]; then
 	cp /usr/share/applications/fusion-icon.desktop /etc/skel/Desktop/
 fi
 
+# EXPERIMENTAL, clean icon cache files
+for file in `find /usr/share/icons -name "icon-theme.cache"`; do
+	rm $file
+done
+
 echo -5 | etc-update
+mount -t proc proc /proc
 /lib/rc/bin/rc-depend -u
 
 echo "Vacuum cleaning client db"
@@ -57,5 +59,18 @@ equo database vacuum
 /etc/init.d/savecache start
 /etc/init.d/savecache zap
 
+ldconfig
+ldconfig
+umount /proc
+
 equo deptest --pretend
 emaint --fix world
+
+# remove anaconda .git
+rm /opt/anaconda/.git -rf
+
+# copy Portage config from sabayonlinux.org entropy repo to system
+cp /var/lib/entropy/client/database/*/sabayonlinux.org/standard/*/*/package.mask /etc/portage/package.mask
+cp /var/lib/entropy/client/database/*/sabayonlinux.org/standard/*/*/package.unmask /etc/portage/package.unmask
+cp /var/lib/entropy/client/database/*/sabayonlinux.org/standard/*/*/package.use /etc/portage/package.use
+cp /var/lib/entropy/client/database/*/sabayonlinux.org/standard/*/*/make.conf /etc/make.conf
